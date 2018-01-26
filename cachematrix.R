fc441473c065b2350240708292e38a00a532c75e
heyimeng97 <35619606+heyimeng97@users.noreply.github.com>
2018-01-26 10:53:14 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-# NOTE:  singular matrices' inverse could not be computed, and the default could be directly used
+        # NOTE:  singular matrices' inverse could not be computed, and the default could be directly used
 #        please run the makeCacheMatrix FIRST, then run cachesolve 
 
-makeCacheMatrix <- function(x = matrix(c(8,3,4,1,5,9,6,7,2),3), Inv = TRUE){  # if you don't want to compute invserse in this function, please 
-        inv <<- NULL                                                          # set Inv = 0. e.g. makeCacheMatrix(x = matrix(...), 0)
+makeCacheMatrix <- function(x = matrix(c(8,3,4,1,5,9,6,7,2),3), Inv = TRUE){  # if you don't want to compute invserse in this 
+        inv <<- NULL                                                          # function, please set Inv = 0. 
         x <<- x  #save matrix x into cache
                 if (Inv == TRUE){
                         inv <<- solve(x)  # save inv(x) into cache 
@@ -13,8 +13,8 @@ makeCacheMatrix <- function(x = matrix(c(8,3,4,1,5,9,6,7,2),3), Inv = TRUE){  # 
 
 
 
-cacheSolve <- function(x) {
-        if(!is.null(inv)){
+cacheSolve <- function(x) {                                            ## This funcion can either compute the value of inverse or load it
+        if(!is.null(inv)){                                             ## from the cache 
                 print('load the value from cache')
                 return(inv)}
         else {
